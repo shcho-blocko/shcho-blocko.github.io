c37c406e29e05dc44ada8278950005107f0d3f34
shcho <shcho@blocko.io>
2016-09-09 13:01:19 +0900

    added coinstack-sample-js/createkey.html

---
+++ inc/coinstack-ksm.js
@@ -0,0 +1,12 @@
+CoinStack.ECKey.createKeyFromHexSeed = function(seed) { 
+   return CoinStack.Util.bitcoin().HDNode.fromSeedHex(seed, CoinStack.Util.bitcoin().networks.bitcoin).privKey.toWIF()
+}
+CoinStack.ECKey.createKeyKSM = function(window) {
+    if (window.crypto || window.msCrypto) {
+        return CoinStack.ECKey.createKey();
+    }
+    console.log("createKeyFromHexSeed()");
+    //return CoinStack.ECKey.createKeyFromHexSeed("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
+    var seed = CoinStack.Util.hashSha256(new String(Date.now()));
+    return CoinStack.ECKey.createKeyFromHexSeed(seed);
+}
